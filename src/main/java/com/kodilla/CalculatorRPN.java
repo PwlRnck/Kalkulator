0261004235b3b4b996fa97e9319feb2523e35d76
“PwlRnck <p.reniecki@gmail.com>
2017-07-02 22:27:42 +0200

    Calculator RPN: regex, input from keyboard

--- src/main/java/com/kodilla/CalculatorRPN.java
+++ src/main/java/com/kodilla/CalculatorRPN.java
@@ -3,6 +3,8 @@ package com.kodilla;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -14,46 +16,57 @@ public class CalculatorRPN {
 
             String temp = "";
             String[] operators = {"+","-","/","*"} ;
-            String[] ignore = {"+","-","/","*",","} ;
+            //String[] ignore = {"+","-","/","*",","} ;
             List<String> lista = new ArrayList<>();
+            int howManyOps = 0;
 
             for(int i=0;i<input.length();i++){
+                Pattern pattern =  Pattern.compile("[0-9]");
+                Matcher matcher = pattern.matcher(input.substring(i,i+1));
 
-                if(!Arrays.asList(ignore).contains(input.substring(i,i+1))){
+                //if(!Arrays.asList(ignore).contains(input.substring(i,i+1))){
+                if(matcher.find()){
                     temp = temp + input.substring(i,i+1);
                 }else if(Arrays.asList(operators).contains(input.substring(i,i+1))){
-                    if(!(temp=="")){
+                    if(!(temp.equals(""))){
                         lista.add(temp);}
                     lista.add(input.substring(i,i+1));
                     temp = "";
+                    howManyOps++;
                 }else{
-                    if(!(temp=="")){
+                    if(!(temp.equals(""))){
                         lista.add(temp);}
                     temp = "";
                 }
             }
-
             return lista;
     }
 
     public double rpnCalc(List<String> lista){
         Deque<Double> kolejka = new ArrayDeque<>();
         String[] operators = {"+","-","/","*"} ;
-        Iterator<String> iter = lista.iterator();
+        double tempA=0;
+        double tempB=0;
 
-        while(iter.hasNext()){
-        //for(String dane:lista){
-            String dane = iter.next();
+        for(String dane:lista){
             if(!Arrays.asList(operators).contains(dane)){
                 kolejka.push((double)Integer.parseInt(dane));
-            }else if(dane == "+"){
-                kolejka.push(kolejka.pop()+kolejka.pop());
-            }else if(dane == "-"){
-                kolejka.push(kolejka.pop()-kolejka.pop());
-            }else if(dane == "*"){
-                kolejka.push(kolejka.pop()*kolejka.pop());
-            }else if(dane == "/"){
-                kolejka.push(kolejka.pop()/kolejka.pop());
+            }else if(dane.equals("+")){
+                tempB = kolejka.pop();
+                tempA = kolejka.pop();
+                kolejka.push(tempA + tempB);
+            }else if(dane.equals("-")){
+                tempB = kolejka.pop();
+                tempA = kolejka.pop();
+                kolejka.push(tempA - tempB);
+            }else if(dane.equals("*")){
+                tempB = kolejka.pop();
+                tempA = kolejka.pop();
+                kolejka.push(tempA * tempB);
+            }else if(dane.equals("/")){
+                tempB = kolejka.pop();
+                tempA = kolejka.pop();
+                kolejka.push(tempA / tempB);
             }
         }
 
@@ -61,8 +74,16 @@ public class CalculatorRPN {
     }
 
     public static void main(String[] args){
-        CalculatorRPN calcRPN = new CalculatorRPN();
-        String input="12,2,3,4,*,10,5,/,+,*,+";
-        System.out.println(calcRPN.rpnCalc(calcRPN.inpToList(input)));
+            CalculatorRPN calcRPN = new CalculatorRPN();
+            String doYouWant="y";
+            Scanner keyboard = new Scanner(System.in);
+            while(doYouWant.equals("y")) {
+                System.out.println("enter your data to calculate in RPN mode; integer numbers only, divide numbers and operators with a comma \",\":");
+                String input = keyboard.nextLine();
+                System.out.println(calcRPN.rpnCalc(calcRPN.inpToList(input)));
+                System.out.println("do you want to make another calculation?(y/n)");
+                doYouWant = keyboard.nextLine();
+            }
+            System.out.println("Thank you for using our calculator");
     }
 }
